d6352b6dfacdd2df50e1310ef2726799a06ec650
mathav95raj <mathav95raj@gmail.com>
2020-09-13 14:58:55 +0530

    tuned the model. added read me and config

---
+++ config.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 13 13:17:50 2020
+
+@author: cosmotron
+"""
+WORD_FREQUENCY = 5
+MAX_NEWS_LENGTH = 25
+
+batch_size = 128
+epochs = 10
+test_size = 20
